e827bf489afeea112e018ab4e173d8fe7b400860
Mafe-SL <fernanda.mariastb@gmail.com>
2024-09-12 15:25:28 -0600

    12 Septiembre

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Descripción del Proyecto: Sistema de Gestión y Agendamiento de Citas Médicas
+Este proyecto tiene como objetivo desarrollar una aplicación móvil integral que permita a los usuarios gestionar, agendar y asistir a citas médicas, tanto de manera online como presencial. La plataforma está diseñada para ofrecer una experiencia eficiente y fluida, conectando a los pacientes con doctores especializados en distintas áreas de la medicina, a través de una interfaz intuitiva y accesible.</div>
+  )
+}
+
+export default page
